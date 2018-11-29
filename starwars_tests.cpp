56f651603d54aff143042edc67b0bf685492ba4e
cstankonrad <cstankonrad@users.noreply.github.com>
2018-11-29 11:46:02 +0100

    battle tests

--- starwars_tests.cpp
+++ starwars_tests.cpp
@@ -6,10 +6,10 @@
 
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/execution_monitor.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include "rebelfleet.h"
 #include "imperialfleet.h"
 #include "battle.h"
-//#include "battle.h"
 #include <cassert>
 
 
@@ -59,7 +59,7 @@ bool checkDamageTake(T &&ship, U dmg, U result) {
     return ship.getShield() == result;
 }
 
-/*
+
 
 BOOST_AUTO_TEST_SUITE(ProvidedTests);
 
@@ -101,7 +101,7 @@ BOOST_AUTO_TEST_SUITE(ProvidedTests);
 
 BOOST_AUTO_TEST_SUITE_END();
 
- */
+
 
 BOOST_AUTO_TEST_SUITE(Rebels);
 
@@ -155,32 +155,93 @@ BOOST_AUTO_TEST_SUITE_END();
 BOOST_AUTO_TEST_SUITE(Battle);
 
     BOOST_AUTO_TEST_CASE(construction) {
-        XWing<float> xwing(100.0f, 300000.0f, 50.0f);
-        Explorer<int> explorer(150, 400000);
-        StarCruiser<unsigned> cruiser(1234, 100000, 11);
-        DeathStar<long> deathStar(10000, 75);
-        TIEFighter<unsigned> fighter(50, 9);
-        ImperialDestroyer<int> destroyer(150, 20);
-        auto battle = SpaceBattle<short,
-                1, 23,
-                DeathStar<long>,
-                Explorer<int>,
-                TIEFighter<unsigned>,
-                XWing<float >>(deathStar,
-                               explorer,
-                               fighter,
-                               xwing);
         {
-            std::vector<short> res{0, 1, 4, 9, 16};
+            XWing<float> xwing(100.0f, 300000.0f, 50.0f);
+            Explorer<int> explorer(150, 400000);
+            StarCruiser<unsigned> cruiser(1234, 100000, 11);
+            DeathStar<long> deathStar(10000, 75);
+            TIEFighter<unsigned> fighter(50, 9);
+            //ImperialDestroyer<int> destroyer(150, 20);
+            auto battle = SpaceBattle<short,
+                    1, 23,
+                    DeathStar<long>,
+                    Explorer<int>,
+                    TIEFighter<unsigned>,
+                    XWing<float >>(deathStar,
+                                   explorer,
+                                   fighter,
+                                   xwing);
+            {
+                std::vector<short> res{0, 1, 4, 9, 16};
+
+                BOOST_REQUIRE_EQUAL(battle.debug_get_attack_moments().size(), res.size());
+
+                for (int i = 0; i < res.size(); ++i) {
+                    BOOST_CHECK_EQUAL(battle.debug_get_attack_moments()[i], res[i]);
+                }
+
+
+                BOOST_CHECK_EQUAL(battle.countRebelFleet(), 2);
+                BOOST_CHECK_EQUAL(battle.countImperialFleet(), 2);
+            }
+        }
+
+        {
+            XWing<float> xwing(100.0f, 300000.0f, 50.0f);
+            XWing<float> xwing2(0.0f, 300000.0f, 50.0f);
+            Explorer<int> explorer(150, 400000);
+            Explorer<float> explorer2(0.0, 400000.0);
+            StarCruiser<unsigned> cruiser(1234, 100000, 11);
+
+            StarCruiser<unsigned> cruiser2(0, 100000, 11);
+            StarCruiser<float> cruiser3(1234, 100000, 11);
+            DeathStar<long> deathStar(10000, 75);
+            TIEFighter<unsigned> fighter(50, 9);
+            TIEFighter<unsigned> fighter2(0, 9);
+            ImperialDestroyer<int> destroyer(150, 20);
+            ImperialDestroyer<int> destroyer2(0, 20);
+
+            auto battle = SpaceBattle<int,
+                    20, 50,
+                    StarCruiser<unsigned>,
+                    StarCruiser<unsigned>,
+                    StarCruiser<float>,
+                    ImperialDestroyer<int>,
+                    ImperialDestroyer<int>,
+                    DeathStar<long>,
+                    Explorer<int>,
+                    Explorer<float>,
+                    TIEFighter<unsigned>,
+                    TIEFighter<unsigned>,
+                    XWing<float >,
+                    XWing<float >>(
+                    cruiser,
+                    cruiser2,
+                    cruiser3,
+                            destroyer,
+                                   destroyer2,
+                            deathStar,
+                                   explorer,
+                                   explorer2,
+                                   fighter,
+                                   fighter2,
+                                   xwing,
+                                   xwing2
+                                   );
+
+            std::vector<int> res{0, 1, 4, 9, 16, 25, 36, 49};
+
+            BOOST_REQUIRE_EQUAL(battle.debug_get_attack_moments().size(), res.size());
 
             for (int i = 0; i < res.size(); ++i) {
                 BOOST_CHECK_EQUAL(battle.debug_get_attack_moments()[i], res[i]);
             }
 
+            BOOST_CHECK_EQUAL(battle.countImperialFleet(), 3);
+            BOOST_CHECK_EQUAL(battle.countRebelFleet(), 4);
 
-            BOOST_CHECK_EQUAL(battle.countRebelFleet(), 2);
-            BOOST_CHECK_EQUAL(battle.countImperialFleet(), 2);
         }
+
 
     }
 
